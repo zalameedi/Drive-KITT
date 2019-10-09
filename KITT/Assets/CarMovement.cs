9f73a1c75c8a035795ab113e60da93855dabaf0f
Zeid Al-Ameedi <zeid.al-ameedi@wsu.edu>
2019-10-08 18:43:22 -0700

    Built KITT and established temporary terrain/physics

---
+++ KITT/Assets/CarMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+
+    //public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, World!");
+        //rb = GetComponent<Rigidbody>();
+        //rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
