f2970e9b3434552d3163f151749e182af0eeffba
Zeid Al-Ameedi <zeid.al-ameedi@wsu.edu>
2019-10-09 20:56:59 -0700

    Added user input for movement and fixed slippery terrain

--- KITT/Assets/CarMovement.cs
+++ KITT/Assets/CarMovement.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class CarMovement : MonoBehaviour
 {
 
-    //public Rigidbody rb;
-
+    public Rigidbody rb;
+    public float forwardForce = 2000;
+    public float YForce = 0;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, World!");
-        //rb = GetComponent<Rigidbody>();
-        //rb.useGravity = false;
+        //Debug.Log("Hello, World!");
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(forwardForce*Time.deltaTime, 0, 0 * Time.deltaTime);
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(0, YForce, 0); // If d is pressed 500 will be added to y axis.
+        }
+
+        if (Input.GetKey("f"))
+        {
+            rb.AddForce(0, -YForce, 0); 
+        }
     }
 }
